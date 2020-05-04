0816b971dd688ab70e62d56a6940bbe9b7fdcfd4
AEdroth <64492026+AEdroth@users.noreply.github.com>
2020-05-04 17:02:52 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function incr() {
+var v1=document.getElementById('pb').value;
+document.getElementById("pb").value= v1 + 1;
+}
+
+function incriii() {
+    var v1=document.getElementById('pb').value;
+    document.getElementById("pb").value= v1 + 3;
+}
+
+function incrvii() {
+    var v1=document.getElementById('pb').value;
+    document.getElementById("pb").value= v1 + 7;
+}
+
+function rst() {
+    var v1=document.getElementById('pb').value;
+    document.getElementById("pb").value= 0;
+}
